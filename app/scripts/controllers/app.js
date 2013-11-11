b23b2d1ce4cd002899bb5a0d196bf446530b7c9e
Murph <pandeiro@gmail.com>
2013-11-11 15:05:37 -0200

    create AppCtrl scaffolding

---
+++ app/scripts/controllers/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chessApp')
+  .controller('AppCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
